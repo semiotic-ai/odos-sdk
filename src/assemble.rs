cf8c89e330b3c0b2d4530ec73ab5c7ea3f895252
Joseph Livesey <joseph@semiotic.ai>
2025-11-06 15:40:32 -0500

    feat: support configuring custom endpoints

--- src/assemble.rs
+++ src/assemble.rs
@@ -6,6 +6,14 @@ use alloy_rpc_types::TransactionRequest;
 use serde::{Deserialize, Serialize};
 
 /// The URL for the Odos Assemble API
+///
+/// # Deprecated
+/// This constant is deprecated. Use [`ClientConfig::assemble_url`] instead
+/// to configure the assemble endpoint URL.
+#[deprecated(
+    since = "0.17.0",
+    note = "Use ClientConfig::assemble_url instead for configurable endpoints"
+)]
 pub const ASSEMBLE_URL: &str = "https://api.odos.xyz/sor/assemble";
 
 /// Request to the Odos Assemble API: <https://docs.odos.xyz/build/api-docs>
